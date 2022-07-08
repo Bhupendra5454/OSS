a8d58a03bb580e01b83f77ee5c17e87b9fd17ecc
Bhupendra <108923402+Bhupendra5454@users.noreply.github.com>
2022-07-08 16:02:03 +0530

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TFLWebApp.Models
+{
+
+    //POCO class
+    //Entity Class
+    //Data Annotations defined metadata for class or field, or property
+
+    [Table("Products")]
+    public class Product
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        [Range(minimum:4, maximum:4000)]
+        public int Quantity { get; set; }
+        
+   
+
+    }
+}
